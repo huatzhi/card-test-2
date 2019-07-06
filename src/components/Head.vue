dae87011b4c1cb226fb435c38d03dc34a687d8ad
Huat Zhi <huatzhi@gmail.com>
2019-07-06 13:44:57 +0800

    initiate header test

---
+++ src/components/Head.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="jumbotron d-flex align-items-center">
+    test test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'open-iconic/font/css/open-iconic-bootstrap.css'
 
 Vue.config.productionTip = false
 
