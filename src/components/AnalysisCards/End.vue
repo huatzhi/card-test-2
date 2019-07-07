2256edfda3fed7a387209aa22f1850b0e083ac9c
Huat Zhi <huatzhi@gmail.com>
2019-07-07 18:34:07 +0800

    fix infinite loop

--- src/components/AnalysisCards/End.vue
+++ src/components/AnalysisCards/End.vue
@@ -17,6 +17,7 @@ export default {
     let currentAnalysisTF = this.$store.state.analysis.currentAnalysisTF;
     if (analysisType === "BB&B" && currentAnalysisTF === "BB") {
       this.$store.dispatch("analysis/append", "\n\nB");
+      this.$store.commit("analysis/setCurrentAnalysisTF", "B")
       this.$emit('changeCard', "SceTrend")
     }
   }
