0d425750a21915da165cd4154be47a5973d80ddb
Huat Zhi <huatzhi@gmail.com>
2019-07-07 18:01:38 +0800

    add keys to prevent unintentionally reuse of card

--- src/components/Analysis.vue
+++ src/components/Analysis.vue
@@ -1,21 +1,23 @@
 <template>
 <div>
-    <Start v-if="currentCardName==='Start'" @changeCard="changeCard" />
-    <SceTrend v-if="currentCardName==='SceTrend'" @changeCard="changeCard" />
-    <Triangle v-if="currentCardName==='Triangle'" @changeCard="changeCard" />
-    <Gartley v-if="currentCardName==='Gartley'" @changeCard="changeCard" />
-    <Cypher v-if="currentCardName==='Cypher'" @changeCard="changeCard" />
-    <HnS v-if="currentCardName==='HnS'" @changeCard="changeCard" />
-    <MultipleTip v-if="currentCardName==='MultipleTip'" @changeCard="changeCard" />
-    <Flag v-if="currentCardName==='Flag'" @changeCard="changeCard" />
-    <CupNHandle v-if="currentCardName==='CupNHandle'" @changeCard="changeCard" />
-    <DSR v-if="currentCardName==='DSR'" @changeCard="changeCard" />
-    <DrawSR v-if="currentCardName==='DrawSR'" @changeCard="changeCard" />
-    <StratShadow v-if="currentCardName==='StratShadow'" @changeCard="changeCard" />
-    <Fibs v-if="currentCardName==='Fibs'" @changeCard="changeCard" />
-    <ControlCandle v-if="currentCardName==='ControlCandle'" @changeCard="changeCard" />
-    <Swing v-if="currentCardName==='Swing'" @changeCard="changeCard" />
-    <TestAnalysisCard v-if="currentCardName==='TestAnalysisCard'" @changeCard="changeCard" />
+    <Start v-if="currentCardName==='Start'" @changeCard="changeCard" :key="key" />
+    <SceTrend v-if="currentCardName==='SceTrend'" @changeCard="changeCard" :key="key" />
+    <Triangle v-if="currentCardName==='Triangle'" @changeCard="changeCard" :key="key" />
+    <Gartley v-if="currentCardName==='Gartley'" @changeCard="changeCard" :key="key" />
+    <Cypher v-if="currentCardName==='Cypher'" @changeCard="changeCard" :key="key" />
+    <HnS v-if="currentCardName==='HnS'" @changeCard="changeCard" :key="key" />
+    <MultipleTip v-if="currentCardName==='MultipleTip'" @changeCard="changeCard" :key="key" />
+    <Flag v-if="currentCardName==='Flag'" @changeCard="changeCard" :key="key" />
+    <CupNHandle v-if="currentCardName==='CupNHandle'" @changeCard="changeCard" :key="key" />
+    <DSR v-if="currentCardName==='DSR'" @changeCard="changeCard" :key="key" />
+    <DrawSR v-if="currentCardName==='DrawSR'" @changeCard="changeCard" :key="key" />
+    <StratShadow v-if="currentCardName==='StratShadow'" @changeCard="changeCard" :key="key" />
+    <Fibs v-if="currentCardName==='Fibs'" @changeCard="changeCard" :key="key" />
+    <ControlCandle v-if="currentCardName==='ControlCandle'" @changeCard="changeCard" :key="key" />
+    <Swing v-if="currentCardName==='Swing'" @changeCard="changeCard" :key="key" />
+    <PriceRelative v-if="currentCardName==='PriceRelative'" @changeCard="changeCard" :key="key" />
+    <Price v-if="currentCardName==='Price'" @changeCard="changeCard" :key="key" />
+    <TestAnalysisCard v-if="currentCardName==='TestAnalysisCard'" @changeCard="changeCard" :key="key" />
 </div>
 </template>
 
@@ -35,6 +37,8 @@ import StratShadow from "./AnalysisCards/StratShadow";
 import Fibs from "./AnalysisCards/Fibs";
 import ControlCandle from "./AnalysisCards/ControlCandle";
 import Swing from "./AnalysisCards/Swing";
+import PriceRelative from "./AnalysisCards/PriceRelative";
+import Price from "./AnalysisCards/Price";
 import TestAnalysisCard from "./AnalysisCards/TestAnalysisCard";
 
 export default {
@@ -60,14 +64,18 @@ export default {
         // close up
         ControlCandle,
         Swing,
+        PriceRelative,
+        Price,
     },
     data() {
-      return {
-        currentCardName: "Start"
-      }
+        return {
+            currentCardName: "Start",
+            key: 0,
+        }
     },
     methods: {
         changeCard(value) {
+            this.key++; // to prevent reuse of analysis card
             this.currentCardName = String(value)
         },
     }
